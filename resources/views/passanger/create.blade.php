819f4a9f318688a5f635a6bf0018996116bd5e83
eszti10 <hefti.king@gmail.com>
2022-11-25 10:20:34 +0100

    1125

--- resources/views/passanger/create.blade.php
+++ resources/views/passanger/create.blade.php
@@ -1,12 +1,15 @@
 @extends("layout")
 @section('content')
-<form action="/utasok" method="POST" name="letrehozo" onSubmit="return validdateForm()">
+<form class="need-validation" novalidate  action="/utasok" method="POST" name="letrehozo" onSubmit="return validdateForm()">
     @csrf
     <div class="form-group">
     <label for="name">Utas neve:</label>
-        <input type="text" class="form-control" name="name"  id="name" placeholder="Utas neve">
+        <input for="validitionCustom1" type="text" class="form-control" name="name"  id="name" placeholder="Utas neve" required>
+        <div class="valid-feedback">
+            Megfelel.
+        </div>
         <label for="name">Szemelyi szám:</label>
-        <input type="text" class="form-control" name="szemszam" id="szemszam" placeholder="Személyi szám">
+        <input type="text" class="form-control" name="szemszam" id="szemszam" placeholder="Személyi szám" required>
         <label for="name">Születési dátum:</label>
         <input type="date" class="form-control" name="szuldat" id="szuldat" placeholder="Születési dátum">
         <label for="name">Repülőgép:</label>
@@ -37,5 +40,11 @@
        }
     }
 </script>
+<script>// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {  'use strict';  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');    // Loop over them and prevent submission
+         var validation = Array.prototype.filter.call(forms, function(form) {      form.addEventListener('submit', function(event) {        if (form.checkValidity() === false) {          event.preventDefault();          event.stopPropagation();        }        form.classList.add('was-validated');      }, false);});}, false);})();
+        </script>
 @endpush
 @endsection
